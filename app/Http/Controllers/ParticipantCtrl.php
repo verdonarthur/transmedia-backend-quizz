4576ecced0bb40caa5c84d5cd9744bac70f7eaba
Arthur <verdon.arthur@gmail.com>
2019-04-10 14:21:44 +0200

    make migration

---
+++ app/Http/Controllers/ParticipantCtrl.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class ParticipantCtrl extends BaseController
+{
+    public function save(){
+
+    }
+
+    public function all(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1/'], function () use ($router) {
+    $router->get('participant', 'ParticipantCtrl@all');
+    $router->get('participant/save', 'ParticipantCtrl@save');
+});
